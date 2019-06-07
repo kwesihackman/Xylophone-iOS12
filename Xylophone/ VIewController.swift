ad5d3a195eb7974c9d2a9710387caa158508c77a
Eric Hackman <kwesihackman@gmail.com>
2019-06-07 19:56:00 +0000

    updated the keys to play the right key after the view is tapped

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
+    let sounds = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,9 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let tagIndex = sender.tag - 1
         
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource:sounds[tagIndex], withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
